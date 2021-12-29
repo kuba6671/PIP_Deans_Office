82c12185aecf523603688d46c7cbaa61c877fbe7
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2021-12-29 20:08:06 +0100

    Updated ProposalView class, Added Proposal class

---
+++ GUI/src/Proposal.java
@@ -0,0 +1,68 @@
+import java.time.LocalDate;
+
+public class Proposal {
+    String proposalName, name, surname, subject, id;
+    LocalDate date;
+    int session, income;
+    Double avg;
+
+    public Proposal(String proposalName, String name, String surname,
+                    String subject, String id, LocalDate date, int session, int income) {
+        this.proposalName = proposalName;
+        this.name = name;
+        this.surname = surname;
+        this.subject = subject;
+        this.id = id;
+        this.date = date;
+        this.session = session;
+        this.income = income;
+    }
+
+    public Proposal(String proposalName, String name, String surname,
+                    String subject, String id, LocalDate date, int session, Double avg) {
+        this.proposalName = proposalName;
+        this.name = name;
+        this.surname = surname;
+        this.subject = subject;
+        this.id = id;
+        this.date = date;
+        this.session = session;
+        this.avg = avg;
+    }
+
+    public String getProposalName() {
+        return proposalName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getSession() {
+        return session;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public Double getAvg() {
+        return avg;
+    }
+}
--- GUI/src/ProposalView.java
+++ GUI/src/ProposalView.java
@@ -1,64 +1,107 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
 
 public class ProposalView extends JFrame implements ActionListener {
     private JComboBox ProposalChoose;
     private JPanel ProposalPanel;
-    private JPanel SocialGrantForm;
-    private JPanel FellowshipForm;
-    private JTextField nameField;
-    private JTextField surnameField;
-    private JTextField subjectField;
-    private JTextField sessionField;
-    private JTextField idField;
-    private JTextField dateField;
-    private JTextField incomeField;
-    private JTextField nameFieldFF;
-    private JTextField surnameFieldFF;
-    private JTextField subjectFieldFF;
-    private JTextField sessionFieldFF;
-    private JTextField idFieldFF;
-    private JTextField dateFieldFF;
-    private JTextField incomeFieldFF;
+    private JPanel ProposalForm;
+    private JTextField name;
+    private JTextField surname;
+    private JTextField subject;
+    private JTextField session;
+    private JTextField id;
+    private JTextField date;
+    private JTextField income;
+    private JTextField avg;
+    private JPanel ButtonPanel;
+    private JButton sendButton;
+    private JButton resetButton;
+    private JPanel avgPanel;
+    private JPanel incomePanel;
 
-    public ProposalView(){
+    public ProposalView() {
         JFrame proposal = this;
-        proposal.setSize(400,400);
+        proposal.setSize(400, 400);
         proposal.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(ProposalPanel);
         //this.pack();
+
         ProposalChoose.addItem("Stypendium socjalne");
         ProposalChoose.addItem("Stypendium naukowe");
         ProposalChoose.addActionListener(this);
 
-        SocialGrantForm.setVisible(true);
+        resetButton.addActionListener(this);
+        sendButton.addActionListener(this);
+
+
+        ProposalForm.setVisible(true);
+        incomePanel.setVisible(true);
         proposal.setVisible(true);
+
+
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        SocialGrantForm.setVisible(false);
-        FellowshipForm.setVisible(false);
+        incomePanel.setVisible(false);
+        avgPanel.setVisible(false);
         String isSelected = (String) ProposalChoose.getSelectedItem();
-        if(isSelected == "Stypendium socjalne") {
-            SocialGrantForm.setVisible(true);
+        if (isSelected == "Stypendium socjalne") {
+            incomePanel.setVisible(true);
+        } else if (isSelected == "Stypendium naukowe") {
+            avgPanel.setVisible(true);
         }
-        else if (isSelected == "Stypendium naukowe"){
-            FellowshipForm.setVisible(true);
+
+        int session=0;
+        int income=0;
+        double avg= 0.0;
+        LocalDate date = null;
+
+        if (e.getSource() == resetButton) {
+            name.setText("");
+            surname.setText("");
+            subject.setText("");
+            this.session.setText("");
+            id.setText("");
+            this.date.setText("");
+            this.income.setText("");
+            this.avg.setText("");
         }
+        else if(e.getSource() == sendButton){
+            if (isSelected == "Stypendium socjalne") {
+
+                if(!this.date.getText().isEmpty()) {
+                    String dateFormString = this.date.getText();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ENGLISH);
+                    date = LocalDate.parse(dateFormString, formatter);
+                }
+               if(!this.session.getText().isEmpty()) {
+                    session = Integer.parseInt(this.session.getText());
+                }
+               if(!this.income.getText().isEmpty()) {
+                   income = Integer.parseInt(this.income.getText());
+               }
+                if(!this.avg.getText().isEmpty()) {
+                     avg = Double.parseDouble(this.avg.getText());
+                }
+
+                Proposal SocialGrantForm = new Proposal("Stypendium socjalne",name.getText(),surname.getText(),
+                        subject.getText(),id.getText(),date,session,income);
+            }
+            else if(isSelected == "Stypendium naukowe"){
+                Proposal FellowShipForm = new Proposal("Stypendium naukowe",name.getText(),surname.getText(),
+                        subject.getText(),id.getText(),date,session,avg);
+            }
+        }
+
     }
 
 
-   /* private void createUIComponents() {
-        //String isSelected = ProposalChoose.getSelectedItem();
-        String isSelected = (String) ProposalChoose.getSelectedItem();
-        /*if(isSelected == "Stypendium socjalne") {
-            System.out.println("Stypendium socjalne");
-        }
-        else if (isSelected == "Stypendium naukowe"){
-            System.out.println("Stypendium naukowe");
-        }
-    }*/
 }
