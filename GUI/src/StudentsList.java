6e8cc824d9ab087f232e581d2d9035e354e5d0b8
Tomek <tomaszchuma18@gmail.com>
2022-01-09 17:10:26 +0100

    Updated StudentsList(sorting by group with combobox)

--- GUI/src/StudentsList.java
+++ GUI/src/StudentsList.java
@@ -16,7 +16,13 @@ public class StudentsList extends JFrame {
     private JScrollPane ScrollStudent;
     private JComboBox GroupChoose;
 
+    private final DefaultTableModel model = new DefaultTableModel(0, 6);
+
+
     String[] header = {"Nr indeksu", "Nazwisko", "Imię", "Nr telefonu","Email","Grupa"};
+
+    private String isSelected;
+
 
 
     public StudentsList(){
@@ -26,32 +32,63 @@ public class StudentsList extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(StudentPanel);
 
+        model.setColumnIdentifiers(header);
+        studentTable = new JTable(model);
 
-        ScrollStudent = new JScrollPane(studentTable);
-        students.add(ScrollStudent);
+
+        groupCombo();
+        makeTable();
 
         //examTable.setDefaultEditor(Object.class, null);
 
+
+        GroupChoose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                remove();
+                StudentsList.this.isSelected = GroupChoose.getSelectedItem().toString();
+                groupSort(isSelected);
+            }
+        });
+
+        ScrollStudent = new JScrollPane(studentTable);
+        students.add(ScrollStudent);
         studentTable.setVisible(true);
         students.setVisible(true);
+
+    }
+
+    private void groupCombo(){
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "system123");
+            Statement stmt = con.createStatement();
+            System.out.println("Connection is created successfully:");
+            ResultSet rs = stmt.executeQuery("select distinct groupid from studenci order by groupid asc");
+            while(rs.next())
+            {
+                GroupChoose.addItem(rs.getInt("groupid"));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-        DefaultTableModel model = new DefaultTableModel(0,6);
-        model.setColumnIdentifiers(header);
+    private void makeTable(){
         try {
-            studentTable = new JTable(model);
+
+
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "system123");
             Statement stmt = con.createStatement();
             System.out.println("Connection is created successfully:");
             ResultSet rs = stmt.executeQuery("select * from studenci");
-            while(rs.next())
-            {
-                Object[] row = {rs.getInt("studentid"),rs.getString("lastname"),rs.getString("firstname"),rs.getInt("phonenumber"),rs.getString("mail"),rs.getInt("groupid")};
+            while (rs.next()) {
+                Object[] row = {rs.getInt("studentid"), rs.getString("lastname"), rs.getString("firstname"), rs.getInt("phonenumber"), rs.getString("mail"), rs.getInt("groupid")};
                 model.addRow(row);
             }
         } catch (ClassNotFoundException e) {
@@ -59,8 +96,39 @@ public class StudentsList extends JFrame {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-
-
     }
+
+    private void groupSort(String group) {
+        try {
+
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "system123");
+            Statement stmt = con.createStatement();
+            System.out.println("Connection is created successfully:");
+            ResultSet rs = stmt.executeQuery("select * from studenci where groupid = " + group);
+            System.out.println("Wywołano z " + group);
+            while (rs.next()) {
+                Object[] row = {rs.getInt("studentid"), rs.getString("lastname"), rs.getString("firstname"), rs.getInt("phonenumber"), rs.getString("mail"), rs.getInt("groupid")};
+                model.addRow(row);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+
+
+        }
+    }
+
+    private void remove() {
+        while (model.getRowCount()> 0) {
+            model.removeRow(0);
+        }
+    }
+
+
+
+
 
 }
